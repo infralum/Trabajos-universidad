0a96366faf3ad0afecc2c8d1094471e1f65d0929
Mauricio Salinas <81931891+infralum@users.noreply.github.com>
2025-04-18 13:49:36 -0400

    Numeros negativos
    Ingresar numeros, hasta que uno sea positivo

---
+++ Numero negativo.py
@@ -0,0 +1,7 @@
+numero = int(input("ingrese un numero: "))
+while numero >= 0:
+    print("el numero es positivo")
+    numero =int(input("vuelva a ingresar un numero: "))
+print("el numero es negativo")
+
+        
