d2f8d1a5f0c0870b81c7987b0f4e7a27050318a9
Mauricio Salinas <81931891+infralum@users.noreply.github.com>
2025-04-18 13:48:03 -0400

    Sumar todos los numeros decididos por el usuario

---
+++ Sumar todos los numeros decididos por el usuario.py
@@ -0,0 +1,11 @@
+n1 = int(input("cuantos numeros va ingresar"))
+contador = 0
+suma = 0
+
+while contador < n1:
+    numeros = int(input("ingrese el numero"))
+    suma = suma + numeros
+    contador += 1
+
+print(f"el total es de {suma}")
+        
